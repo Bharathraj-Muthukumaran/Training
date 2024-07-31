d86e7d5c7141ff70bfe1496c0b0b8b1b5eb556b0
Bharathraj M <[bharathmuthukumaran17@gmail.com]>
2024-07-31 18:53:03 +0530

    updated angular

--- Frontend/Angular/Day1/Exercise/MyAngularProject1/src/app/childr/childr.component.ts
+++ Frontend/Angular/Day1/Exercise/MyAngularProject1/src/app/childr/childr.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CalculatorService } from '../calculator.service';
 
 @Component({
   selector: 'app-childr',
@@ -6,18 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './childr.component.css'
 })
 export class ChildrComponent {
-  flag : boolean;
+  /*flag : boolean;
   vehicles : string[];
   selectedVehicle : string;
-  myStyle :{};
+  myStyle :{};*/
+  sum : number;
+  sub : number;
+  constructor(private calc : CalculatorService, private fact : FactorialService ){
+    this.sum = calc.getAddition(10,20);
+    this.sub = calc.getSubtraction(10,20);
+  }
 
-  constructor() {
+  /*constructor() {
     this.flag = true;
     this.vehicles = ["TwoWheeler","ThreeWheeler","FourWheeler"];
     this.selectedVehicle ="";
     this.myStyle = {'width' : '40%','border' : '2px solid green','border-radius' : '25px'};
-  }
-  changeFlag(){
+  }*/
+    /*changeFlag(){
     this.flag = ! this.flag;
   }
   setSelectedItem(vec : string){
@@ -25,5 +32,5 @@ export class ChildrComponent {
   }
   changeStyle(){
     this.myStyle ={'width' : '40%','border' : '2px solid red','border-radius' : '25px' };
-  }
+  }*/
 }
